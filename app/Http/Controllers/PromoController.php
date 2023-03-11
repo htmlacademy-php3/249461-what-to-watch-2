182de936ff63ae7e59a47c827a0c45cdffa1bb5e
s.kravtsov <s.kravtsov@hotmail.com>
2023-03-11 20:23:54 +0200

    контроллеры и роуты

---
+++ app/Http/Controllers/PromoController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PromoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Add a newly created resource in storage.
+     */
+    public function add(Request $request)
+    {
+        //
+    }
+}
